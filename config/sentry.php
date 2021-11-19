8a90e710daafb5729764e402244e555617d86893
Rob Trehy <robert.trehy@outlook.com>
2021-11-19 14:30:27 +0000

    Updated Sentry Configuration

--- config/sentry.php
+++ config/sentry.php
@@ -1,5 +1,7 @@
 <?php
 
+use RobTrehy\LaravelApplicationSettings\ApplicationSettings;
+
 $webapps = json_decode(file_get_contents(storage_path('webapps/core/webapps.json')), true);
 
 return [
@@ -13,6 +15,8 @@ return [
     // When left empty or `null` the Laravel environment will be used
     'environment' => env('SENTRY_ENVIRONMENT'),
 
+    'server_name' => '',
+
     'breadcrumbs' => [
         // Capture Laravel logs in breadcrumbs
         'logs' => true,
@@ -21,7 +25,7 @@ return [
         'sql_queries' => true,
 
         // Capture bindings on SQL queries logged in breadcrumbs
-        'sql_bindings' => true,
+        'sql_bindings' => false,
 
         // Capture queue job information in breadcrumbs
         'queue_info' => true,
@@ -53,7 +57,8 @@ return [
     // @see: https://docs.sentry.io/platforms/php/configuration/options/#send-default-pii
     'send_default_pii' => false,
 
-    'traces_sample_rate' => (float)(env('SENTRY_TRACES_SAMPLE_RATE', 0.25)),
+    'traces_sample_rate' => (float)(env('SENTRY_TRACES_SAMPLE_RATE', 0.0)),
+    // 'traces_sample_rate' => (float)(ApplicationSettings::get('core.analytics.optIn') ? 0.25 : 0.0),
 
     'controllers_base_namespace' => env('SENTRY_CONTROLLERS_BASE_NAMESPACE', 'App\\Http\\Controllers'),
 
