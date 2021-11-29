ac02c83b9198eb853ba2c5469278981a0e702d14
Rob Trehy <robert.trehy@outlook.com>
2021-11-29 16:48:02 +0000

    Dark Mode UI Improvements

--- resources/js/widgets/BlockCount.js
+++ resources/js/widgets/BlockCount.js
@@ -23,7 +23,7 @@ const BlockCount = () => {
     }
 
     return (
-        <div className="bg-indigo-300 px-4 w-full h-full rounded-md shadow-inner">
+        <div className="bg-indigo-300 dark:bg-indigo-800 px-4 w-full h-full rounded-md shadow-inner">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 text-white text-right inline-block mt-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z" />
             </svg>
