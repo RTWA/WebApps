5d2933ba3afe3a936e0a1d72801998f09ba28183
Rob Trehy <robert.trehy@outlook.com>
2021-12-07 15:00:15 +0000

    Apps & Plugins settings updated responsive UI

--- resources/js/components/Routes/Settings/AppsPlugins.js
+++ resources/js/components/Routes/Settings/AppsPlugins.js
@@ -4,22 +4,22 @@ import { AppCard, Button, PluginCard, withWebApps } from 'webapps-react';
 const AppPlugins = ({ apps, plugins }) => {
     return (
         <>
-            <Button to="/settings" style="link" className="flex flex-auto -mt-8 text-sm uppercase">
+            <Button to="/settings" style="link" className="flex flex-auto -mt-8 -ml-4 text-sm uppercase">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
                 </svg>
                 Back to settings
             </Button>
-            <div className="w-full md:px-4 md:py-6">
-                <div className="flex flex-row mt-8">
-                    <h6 className="text-gray-600 dark:text-gray-400 text-2xl font-bold md:ml-6">Apps</h6>
+            <div className="w-full py-4">
+                <div className="flex flex-row mt-8 mb-4">
+                    <h6 className="text-gray-600 dark:text-gray-400 text-2xl font-bold">Apps</h6>
                     <div className="flex-1 text-right mt-2">
                         <Button to="/settings/online/apps" square>
                             Get More Apps
                         </Button>
                     </div>
                 </div>
-                <div className="grid grid-cols-1 gap-4 md:grid-cols-5 py-4 px-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
                     {
                         (apps.local === null || apps.local === undefined || typeof apps !== 'object' || apps.local.length === 0) ?
                             (
@@ -41,15 +41,15 @@ const AppPlugins = ({ apps, plugins }) => {
                     }
                 </div>
 
-                <div className="flex flex-row mt-8">
-                    <h6 className="text-gray-600 dark:text-gray-400 text-2xl font-bold md:ml-6">Plugins</h6>
+                <div className="flex flex-row mt-8 mb-4">
+                    <h6 className="text-gray-600 dark:text-gray-400 text-2xl font-bold">Plugins</h6>
                     <div className="flex-1 text-right mt-2">
                         <Button to="/settings/online/plugins" square>
                             Get More Plugins
                         </Button>
                     </div>
                 </div>
-                <div className="grid grid-cols-1 gap-4 md:grid-cols-5 py-4 px-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
                     {
                         (plugins.all === null || plugins.all === undefined || typeof plugins !== 'object' || plugins.all.length === 0) ?
                             (
--- resources/js/components/Routes/Settings/Online/Apps.js
+++ resources/js/components/Routes/Settings/Online/Apps.js
@@ -9,7 +9,7 @@ const Apps = ({ apps }) => {
     if (apps.online === null) {
         return (
             <section>
-                <Button to="/settings/appsplugins" style="link" className="flex flex-auto -mt-8 text-sm uppercase">
+                <Button to="/settings/appsplugins" style="link" className="flex flex-auto -mt-8 -ml-4 text-sm uppercase">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
                     </svg>
@@ -23,14 +23,14 @@ const Apps = ({ apps }) => {
 
     return (
         <section>
-            <Button to="/settings/appsplugins" style="link" className="flex flex-auto -mt-8 text-sm uppercase">
+            <Button to="/settings/appsplugins" style="link" className="flex flex-auto -mt-8 -ml-4 text-sm uppercase">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
                 </svg>
                 Back to Apps & Plugins
             </Button>
             <h2 className="mb-2 text-center text-2xl">Choose an App to install</h2>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-5 py-4 px-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
                 {
                     Object.keys(apps.online).map((key, i) => {
                         return <AppCard key={i} app={apps.online[key]} showActions={true} />
