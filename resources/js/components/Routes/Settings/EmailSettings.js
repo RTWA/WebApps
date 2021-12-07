962833dcf32545c5c0fbadbb548e096192efea4d
Rob Trehy <robert.trehy@outlook.com>
2021-12-07 14:48:41 +0000

    EmailSettings form items updated reponsive

--- resources/js/components/Routes/Settings/EmailSettings.js
+++ resources/js/components/Routes/Settings/EmailSettings.js
@@ -15,7 +15,7 @@ const EmailSettings = ({ user, UI, ...props }) => {
 
     const { addToast } = useToasts();
 
-    const [testTo, setTestTo] = useState(/* istanbul ignore next */ (user) ? user.email : '');
+    const [testTo, setTestTo] = useState(/* istanbul ignore next */(user) ? user.email : '');
 
     const onChange = e => {
         let key = e.target.id;
@@ -59,115 +59,99 @@ const EmailSettings = ({ user, UI, ...props }) => {
     return (
         <>
 
-            <div className="grid grid-cols-6 gap-6 py-4">
-                <div className="col-span-6 md:grid md:grid-cols-4 md:gap-4">
-                    <label className="font-medium text-sm md:text-base" htmlFor="mail.smtp.host">SMTP Server Host</label>
-                    <Input name="mail.smtp.host"
-                        type="text"
-                        id="mail.smtp.host"
-                        value={settings['mail.smtp.host'] || ''}
-                        onChange={onType}
-                        onBlur={onChange}
-                        state={states['mail.smtp.host']} />
-                </div>
+            <div className="flex flex-col xl:flex-row py-4">
+                <label className="w-full xl:w-4/12 xl:py-2 font-medium xl:font-normal text-sm xl:text-base" htmlFor="mail.smtp.host">SMTP Server Host</label>
+                <Input name="mail.smtp.host"
+                    type="text"
+                    id="mail.smtp.host"
+                    value={settings['mail.smtp.host'] || ''}
+                    onChange={onType}
+                    onBlur={onChange}
+                    state={states['mail.smtp.host']} />
             </div>
 
-            <div className="grid grid-cols-6 gap-6 py-4">
-                <div className="col-span-6 md:grid md:grid-cols-4 md:gap-4">
-                    <label className="font-medium text-sm md:text-base" htmlFor="mail.smtp.port">SMTP Server Port</label>
-                    <Input name="mail.smtp.port"
-                        type="text"
-                        id="mail.smtp.port"
-                        value={settings['mail.smtp.port'] || ''}
-                        onChange={onType}
-                        onBlur={onChange}
-                        state={states['mail.smtp.port']} />
-                </div>
+            <div className="flex flex-col xl:flex-row py-4">
+                <label className="w-full xl:w-4/12 xl:py-2 font-medium xl:font-normal text-sm xl:text-base" htmlFor="mail.smtp.port">SMTP Server Port</label>
+                <Input name="mail.smtp.port"
+                    type="text"
+                    id="mail.smtp.port"
+                    value={settings['mail.smtp.port'] || ''}
+                    onChange={onType}
+                    onBlur={onChange}
+                    state={states['mail.smtp.port']} />
             </div>
 
-            <div className="grid grid-cols-6 gap-6 py-4">
-                <div className="col-span-6 md:grid md:grid-cols-4 md:gap-4">
-                    <label className="font-medium text-sm md:text-base" htmlFor="mail.smtp.encryption">SMTP Server Encryption</label>
-                    <select name="mail.smtp.encryption"
-                        id="mail.smtp.encryption"
-                        value={settings['mail.smtp.encryption']}
-                        className={`input-field focus:border-${UI.theme}-600 dark:focus:border-${UI.theme}-500`}
-                        onChange={onChange}>
-                        <option value="">None</option>
-                        <option value="tls">TLS</option>
-                        <option value="ssl">SSL</option>
-                    </select>
-                </div>
+            <div className="flex flex-col xl:flex-row py-4">
+                <label className="w-full xl:w-4/12 xl:py-2 font-medium xl:font-normal text-sm xl:text-base" htmlFor="mail.smtp.encryption">SMTP Server Encryption</label>
+                <select name="mail.smtp.encryption"
+                    id="mail.smtp.encryption"
+                    value={settings['mail.smtp.encryption']}
+                    className={`input-field focus:border-${UI.theme}-600 dark:focus:border-${UI.theme}-500`}
+                    onChange={onChange}>
+                    <option value="">None</option>
+                    <option value="tls">TLS</option>
+                    <option value="ssl">SSL</option>
+                </select>
             </div>
 
-            <div className="grid grid-cols-6 gap-6 py-4">
-                <div className="col-span-6 md:grid md:grid-cols-4 md:gap-4">
-                    <label className="font-medium text-sm md:text-base" htmlFor="mail.smtp.from_address">SMTP From E-Mail Address</label>
-                    <Input name="mail.smtp.from_address"
-                        type="text"
-                        id="mail.smtp.from_address"
-                        value={settings['mail.smtp.from_address'] || ''}
-                        onChange={onType}
-                        onBlur={onChange}
-                        state={states['mail.smtp.from_address']} />
-                </div>
+            <div className="flex flex-col xl:flex-row py-4">
+                <label className="w-full xl:w-4/12 xl:py-2 font-medium xl:font-normal text-sm xl:text-base" htmlFor="mail.smtp.from_address">SMTP From E-Mail Address</label>
+                <Input name="mail.smtp.from_address"
+                    type="text"
+                    id="mail.smtp.from_address"
+                    value={settings['mail.smtp.from_address'] || ''}
+                    onChange={onType}
+                    onBlur={onChange}
+                    state={states['mail.smtp.from_address']} />
             </div>
 
-            <div className="grid grid-cols-6 gap-6 py-4">
-                <div className="col-span-6 md:grid md:grid-cols-4 md:gap-4">
-                    <label className="font-medium text-sm md:text-base" htmlFor="mail.smtp.from_name">SMTP From E-Mail Name</label>
-                    <Input name="mail.smtp.from_name"
-                        type="text"
-                        id="mail.smtp.from_name"
-                        value={settings['mail.smtp.from_name'] || ''}
-                        onChange={onType}
-                        onBlur={onChange}
-                        state={states['mail.smtp.from_name']} />
-                </div>
+            <div className="flex flex-col xl:flex-row py-4">
+                <label className="w-full xl:w-4/12 xl:py-2 font-medium xl:font-normal text-sm xl:text-base" htmlFor="mail.smtp.from_name">SMTP From E-Mail Name</label>
+                <Input name="mail.smtp.from_name"
+                    type="text"
+                    id="mail.smtp.from_name"
+                    value={settings['mail.smtp.from_name'] || ''}
+                    onChange={onType}
+                    onBlur={onChange}
+                    state={states['mail.smtp.from_name']} />
             </div>
 
-            <div className="grid grid-cols-6 gap-6 py-4">
-                <div className="col-span-6 md:grid md:grid-cols-4 md:gap-4">
-                    <label className="font-medium text-sm md:text-base" htmlFor="mail.smtp.username">SMTP Username</label>
-                    <Input name="mail.smtp.username"
-                        type="text"
-                        id="mail.smtp.username"
-                        value={settings['mail.smtp.username'] || ''}
-                        onChange={onType}
-                        onBlur={onChange}
-                        state={states['mail.smtp.username']} />
-                </div>
+            <div className="flex flex-col xl:flex-row py-4">
+                <label className="w-full xl:w-4/12 xl:py-2 font-medium xl:font-normal text-sm xl:text-base" htmlFor="mail.smtp.username">SMTP Username</label>
+                <Input name="mail.smtp.username"
+                    type="text"
+                    id="mail.smtp.username"
+                    value={settings['mail.smtp.username'] || ''}
+                    onChange={onType}
+                    onBlur={onChange}
+                    state={states['mail.smtp.username']} />
             </div>
-            <div className="grid grid-cols-6 gap-6 py-4">
-                <div className="col-span-6 md:grid md:grid-cols-4 md:gap-4">
-                    <label className="font-medium text-sm md:text-base" htmlFor="mail.smtp.password">SMTP Password</label>
-                    <Input name="mail.smtp.password"
-                        type="password"
-                        id="mail.smtp.password"
-                        value={settings['mail.smtp.password'] || ''}
-                        onChange={onType}
-                        onBlur={onChange}
-                        states={states['mail.smtp.password']} />
-                </div>
+            <div className="flex flex-col xl:flex-row py-4">
+                <label className="w-full xl:w-4/12 xl:py-2 font-medium xl:font-normal text-sm xl:text-base" htmlFor="mail.smtp.password">SMTP Password</label>
+                <Input name="mail.smtp.password"
+                    type="password"
+                    id="mail.smtp.password"
+                    value={settings['mail.smtp.password'] || ''}
+                    onChange={onType}
+                    onBlur={onChange}
+                    states={states['mail.smtp.password']} />
             </div>
             <div className="h-px bg-gray-300 dark:bg-gray-700 my-4" />
-            <div className="grid grid-cols-6 gap-6 py-4">
-                <div className="col-span-6 md:grid md:grid-cols-4 md:gap-4">
-                    <label className="font-medium text-sm md:text-base" htmlFor="testTo">Send test to</label>
-                    <div className="relative">
-                        <Input name="testTo"
-                            type="text"
-                            id="testTo"
-                            value={testTo || ''}
-                            onChange={updateTestTo} />
+            <div className="flex flex-col xl:flex-row py-4">
+                <label className="w-full xl:w-4/12 xl:py-2 font-medium xl:font-normal text-sm xl:text-base" htmlFor="testTo">Send test to</label>
+                <div className="relative w-full">
+                    <Input name="testTo"
+                        type="text"
+                        id="testTo"
+                        value={testTo || ''}
+                        onChange={updateTestTo} />
 
-                        <div className="absolute inset-y-0 right-0 flex items-center">
-                            <Button style="ghost" color="gray" size="small"
-                                className="uppercase mr-1"
-                                onClick={sendTest}>
-                                {testSndBtnText}
-                            </Button>
-                        </div>
+                    <div className="w-full sm:w-auto sm:absolute inset-y-0 right-0 sm:flex items-center">
+                        <Button style="ghost" color="gray" size="small" square
+                            className="uppercase mr-1 w-full sm:w-auto sm:rounded-md"
+                            onClick={sendTest}>
+                            {testSndBtnText}
+                        </Button>
                     </div>
                 </div>
             </div>
