b23d04dbaeb42f4dc29055da174e5134d203e8f1
Rob Trehy <robert.trehy@outlook.com>
2022-02-21 14:19:58 +0000

    Added missing import!

--- resources/js/components/Routes/Settings/GroupSearch/GroupSearch.js
+++ resources/js/components/Routes/Settings/GroupSearch/GroupSearch.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Input } from 'webapps-react';
 
 const GroupSearch = ({ id, groupData, setData, accessToken, saveChange, ...props }) => {
