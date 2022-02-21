812a9adc5d37a1ff90341f73e1927e6525311d35
Rob Trehy <robert.trehy@outlook.com>
2022-02-21 12:23:36 +0000

    Catch Curl Error

--- app/Http/Controllers/MSGraphController.php
+++ app/Http/Controllers/MSGraphController.php
@@ -66,8 +66,10 @@ class MSGraphController extends Controller
         ]);
         $content = json_decode(curl_exec($curl), true);
         curl_close($curl);
-        // TODO: Should handle errors in this
-
+        if (isset($content['error'])) {
+            abort(500, $content['error_description']);
+        }
+        
         $token = MSGraphToken::create([
             'access_token' => $content['access_token'],
             'expires' => (time() + $content['expires_in']),
