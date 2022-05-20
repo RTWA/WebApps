5dfcdeaa7bc57239b59a21e7c68784dc032a0415
Rob Trehy <robert.trehy@outlook.com>
2022-05-20 16:23:06 +0100

    Switch Use/Edit Block Button

--- resources/js/components/Routes/Blocks/EditBlock.js
+++ resources/js/components/Routes/Blocks/EditBlock.js
@@ -361,12 +361,24 @@ const EditBlock = props => {
                         </svg>
                         Block Properties
                     </Button>
-                    <Button type="link" className="flex flex-row items-center gap-2 font-normal" onClick={toggleUseBlock}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
-                        </svg>
-                        Use Block
-                    </Button>
+                    {
+                        (!isUseBlock)
+                            ? (
+                                <Button type="link" className="flex flex-row items-center gap-2 font-normal" onClick={toggleUseBlock}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+                                    </svg>
+                                    Use Block
+                                </Button>
+                            ) : (
+                                <Button type="link" className="flex flex-row items-center gap-2 font-normal" onClick={toggleUseBlock}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                    </svg>
+                                    Edit Block
+                                </Button>
+                            )
+                    }
                 </div>
                 <div className="flex flex-col flex-col-reverse gap-y-4 gap-x-6 xl:flex-row w-full">
                     <div className={`${(flyout.opened) ? 'w-0 hidden' : 'w-full xl:w-5/12'}`}>
