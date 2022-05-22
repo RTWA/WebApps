8c5fc214317edd3b03609fc43147347ad5a6bee4
Rob Trehy <robert.trehy@outlook.com>
2022-05-22 17:56:00 +0100

    Hide pinned/bookmarked icon until functionality exists

--- resources/js/components/Routes/Blocks/BlockViews/BlockCard.js
+++ resources/js/components/Routes/Blocks/BlockViews/BlockCard.js
@@ -22,7 +22,7 @@ const BlockCard = ({ block, curBlock, rename, renameBlock, saveName, contextDele
                                 onChange={renameBlock} onBlur={() => { saveName(block) }} />
                         </div>
                     </div>
-                    {
+                    {/* {
                         (block.is_pinned)
                             ? (
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-orange-500 fill-orange-500 cursor-pointer" fill="none" viewBox="0 0 24 24" strokeWidth={2}>
@@ -34,7 +34,7 @@ const BlockCard = ({ block, curBlock, rename, renameBlock, saveName, contextDele
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
                                 </svg>
                             )
-                    }
+                    } */}
                 </div>
             </div>
             <div className="w-54 h-54 overflow-hidden">
