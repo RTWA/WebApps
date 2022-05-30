c28a05e8dd4491e5f6cc4aecddacb42d0ec69924
Rob Trehy <robert.trehy@outlook.com>
2022-05-30 10:26:09 +0100

    Remove unused path

--- resources/js/components/Routes/Settings/Lists/GroupList.js
+++ resources/js/components/Routes/Settings/Lists/GroupList.js
@@ -10,23 +10,19 @@ const GroupList = props => {
     return (
         <div className="w-full dark:text-white divide-y dark:divide-gray-800 border-t border-b dark:border-gray-800">
             {
-                (groups.length === 0) ?
-                    null
-                    : (
-                        groups.map(function (group, i) {
-                            return (
-                                <div key={i} className="grid grid-cols-2 sm:grid-cols-3 bg-white dark:bg-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer" onClick={() => selectGroup(group)}>
-                                    <div className="col-span-2 pl-4 flex flex-row items-center">
-                                        <div className="flex-grow-0 pr-5">
-                                            <UserAvatar size="48" name={group.name} src={`/group/${group.name}/photo`} />
-                                        </div>
-                                        <h6 className="py-3">{group.name}</h6>
-                                    </div>
-                                    <div className="hidden sm:block py-5">{group.users_count} Members</div>
+                groups?.map(function (group, i) {
+                    return (
+                        <div key={i} className="grid grid-cols-2 sm:grid-cols-3 bg-white dark:bg-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer" onClick={() => selectGroup(group)}>
+                            <div className="col-span-2 pl-4 flex flex-row items-center">
+                                <div className="flex-grow-0 pr-5">
+                                    <UserAvatar size="48" name={group.name} src={`/group/${group.name}/photo`} />
                                 </div>
-                            )
-                        })
+                                <h6 className="py-3">{group.name}</h6>
+                            </div>
+                            <div className="hidden sm:block py-5">{group.users_count} Members</div>
+                        </div>
                     )
+                })
             }
         </div>
     )
