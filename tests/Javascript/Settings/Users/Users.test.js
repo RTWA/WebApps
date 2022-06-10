345c2a414ba32f323c4f7bffd3cbbb7b0b628cd9
Rob Trehy <robert.trehy@outlook.com>
2022-06-10 15:13:43 +0100

    Remove wait for disabled users

--- tests/Javascript/Settings/Users/Users.test.js
+++ tests/Javascript/Settings/Users/Users.test.js
@@ -9,7 +9,7 @@ import UsersGroups from '../../../../resources/js/components/Routes/Settings/Use
 describe('UsersGroups Component - Users', () => {
     test('Renders User list', async () => {
         render(<WebAppsUX><BrowserRouter><UsersGroups groups={mockData.groups} /></BrowserRouter></WebAppsUX>);
-        await waitFor(() => screen.getByRole('button', { name: /show disabled users \(2\)/i }));
+        await waitFor(() => screen.getByRole('button', { name: /show disabled users/i }));
 
         expect(screen.getByRole('heading', { name: /users/i })).toBeDefined();
         expect(screen.getByRole('heading', { name: /groups/i })).toBeDefined();
