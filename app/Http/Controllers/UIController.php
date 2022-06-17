edc58f1a96fca090b8a9aef3e272515931244edc
Robert Trehy <robert.trehy@outlook.com>
2022-06-17 20:55:15 +0100

    Nav path for end

--- app/Http/Controllers/UIController.php
+++ app/Http/Controllers/UIController.php
@@ -35,7 +35,7 @@ class UIController extends Controller
             $nav[] = [
                 '_tag' => 'NavItem',
                 'name' => 'My Blocks',
-                'to' => '/blocks',
+                'to' => '/blocks/',
                 'icon' => '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
                         </svg>',
