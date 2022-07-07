15c0f8a70128a64e0f73ecae117de50023656cff
Robert Trehy <robert.trehy@outlook.com>
2022-07-07 14:15:41 +0100

    Add notifications container

--- resources/js/components/Layouts/Default/Default.js
+++ resources/js/components/Layouts/Default/Default.js
@@ -4,6 +4,8 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import {
     AppError,
     Headerbar,
+    Notifications,
+    NotificationsProvider,
     Sidebar,
     WebAppsUXContext
 } from 'webapps-react';
@@ -23,39 +25,44 @@ const Default = () => {
 
     return (
         <div className="flex md:flex-row flex-col h-full">
-            <Sidebar />
-            <AppError theme={theme}>
-                <div className="flex flex-col flex-auto w-full min-w-0 h-full overflow-hidden" id="app-content">
-                    <Headerbar>
-                        <button
-                            className="cursor-pointer text-gray-600 dark:text-gray-400 text-xl leading-none bg-transparent outline-none"
-                            type="button"
-                            onClick={toggleNavigation}
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                            </svg>
-                        </button>
-                    </Headerbar>
-                    <div className="flex flex-col flex-auto overflow-hidden">
-                        <Switch>
-                            {
-                                navigation.routes?.map((route, idx) => {
-                                    let C = RouteComponents[route.component];
-                                    return route.component ? (
-                                        <Route key={idx} path={route.path} exact={route.exact} name={route.name}
-                                            render={props => (
-                                                (C !== undefined) ? <C routedata={route} {...props} />
-                                                    : <div>Error: Component '{route.component}' not found!</div>
-                                            )} />
-                                    ) : (null);
-                                })
-                            }
-                            <Redirect from="/" to="/dashboard" exact />
-                        </Switch>
+            <NotificationsProvider>
+                <Sidebar />
+                <AppError theme={theme}>
+                    <div className="flex flex-col flex-auto w-full min-w-0 h-full overflow-hidden" id="app-content">
+                        <Headerbar>
+                            <button
+                                className="cursor-pointer text-gray-600 dark:text-gray-400 text-xl leading-none bg-transparent outline-none"
+                                type="button"
+                                onClick={toggleNavigation}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                                </svg>
+                            </button>
+                            <div className="ml-auto">
+                                <Notifications />
+                            </div>
+                        </Headerbar>
+                        <div className="flex flex-col flex-auto overflow-hidden">
+                            <Switch>
+                                {
+                                    navigation.routes?.map((route, idx) => {
+                                        let C = RouteComponents[route.component];
+                                        return route.component ? (
+                                            <Route key={idx} path={route.path} exact={route.exact} name={route.name}
+                                                render={props => (
+                                                    (C !== undefined) ? <C routedata={route} {...props} />
+                                                        : <div>Error: Component '{route.component}' not found!</div>
+                                                )} />
+                                        ) : (null);
+                                    })
+                                }
+                                <Redirect from="/" to="/dashboard" exact />
+                            </Switch>
+                        </div>
                     </div>
-                </div>
-            </AppError>
+                </AppError>
+            </NotificationsProvider>
         </div>
     )
 }
